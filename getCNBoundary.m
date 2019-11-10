c6a03d5f7731879db5ef8200f7ae683a928bbb17
Ronald <rmjch91@gmail.com>
2019-11-10 17:15:12 -0600

    I added  complext network and fractal descriptors

--- getCNBoundary.m
+++ getCNBoundary.m
@@ -1,3 +1,45 @@
-function [cnBoundary] = getCNBoundary(boundary)
-    
+function [descriptors] = getCNBoundary(boundary)
+    %Thresholding
+thresholding = 0.025:0.075:0.95;
+
+[coord(:,1) coord(:,2)] = find(img>0);
+
+%network modeling
+cn = cnMake(coord);
+
+%CN evoluting with the thresholding selection
+grade = thresholdingCN(cn,thresholding);
+
+%grade normalizadion
+grade = grade ./ length(grade);
+
+%calculate mean and maximum
+descriptors = mean(grade');
+descriptors = [descriptors max(grade')];
+
+end
+
+
+
+%--------------------------------------------
+function [CN] = cnMake(coord)
+
+[nx ny] = size(coord);
+
+y = pdist(coord,'euclidean');
+CN = squareform(y);
+
+CN = CN ./ max(CN(:));
+
+
+%--------------------------------------------
+function grade = thresholdingCN(cn,thre)
+
+cnU = zeros(size(cn));
+grade = [];
+
+for x=1:length(thre)
+    c = cn < thre(x);
+    cnU(c) = 1;
+    grade(x,:) = sum(cnU) - 1;
 end
--- getFractalBoundary.m
+++ getFractalBoundary.m
@@ -1,3 +1,32 @@
 function [fractalBoundary] = getFractalBoundary(boundary)
-    
+    % Performing Box Counting...
+	for r=2:7
+		rc = @(x) floor(((max(x)-min(x))/r))+ 1; % non-linear filter
+		F= colfilt(boundary, [r r],'sliding', rc);
+		B{r}= log(double(F * (49/(r^2))));
+	end
+	close(h)
+
+	i=log(2:7); % Normalised scale range vector
+
+	%------- computing the slope using linear regression -------%
+	Nxx=dot(i,i)-(sum(i)^2)/6;
+	h = waitbar(0,'Transforming to FD...');
+	for m = 1:M
+		for n = 1:N
+			fd= [B{7}(m,n), B{6}(m,n), B{5}(m,n), B{4}(m,n), B{3}(m,n), B{2}(m,n)]; % Number of boxes multiscale vector
+			Nxy=dot(i,fd)-(sum(i)*sum(fd))/6; 
+			FD{j}(m, n)= (Nxy/Nxx); % slope of the linear regression line
+		end
+		waitbar(m/M)
+	end
+	close(h)
+	end
+	
+	% Computer Multi scale gradient descent
+	
+    fractalBoundary=-gradient(log(size(B,1)))./gradient(log(B));
+    semilogx(B, fractalBoundary, 's-');
+    ylim([0 dim]);
+    xlabel('r, box size'); ylabel('- d ln n / d ln r, local dimension');
 end
