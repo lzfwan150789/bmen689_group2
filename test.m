c40b0c804e2f478494b348ee703bebed82b85ace
nanditac-TAMU <nanditac@tamu.edu>
2019-11-15 18:17:35 -0600

    Updated the file path and checked Nianchao's circularity code for the infinity number for 8.tif (floodfill).

--- test.m
+++ test.m
@@ -1,4 +1,4 @@
 clear all
-I = imread('C:/Users/lenovo/Downloads/Lung Training/Interior/1.tif');
+I = imbinarize(imread('C:/Users/lenovo/Documents/Github/bmen689_group2/Training images/Renamed Clean Set/Boundaries/9.tif'));
 image = I(:, :, 1);
-v = GLCM(image);
+v = Circularity(image);
