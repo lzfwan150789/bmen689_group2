e006faac917af4230cc929518e259ac228f56326
nanditac-TAMU <nanditac@tamu.edu>
2019-11-17 20:49:22 -0600

    use this to generate textures features for fractals

--- getFractalTexture.m
+++ getFractalTexture.m
@@ -1,3 +1,13 @@
-function [fractalTexture] = getFractalTexture(interior)
+function [avg, stddev, median, corrcoeff, skew, kurtosis, mean_df, std_df] = getFractalTexture(interior)
+    avg = mean2(interior);
+    stddev = std2(interior); %2nd moment
+    median = medfilt2(interior);
+    corrcoeff = corr2(interior, median);
+    skew = skewness(interior, 1, 'all'); %3rd moment
+    kurtosis = kurtosis(interior, 1, 'all'); %4th moment
     
+    [n, r] = boxcount(interior, 'slope');
+    df = -diff(log(n))./diff(log(r));
+    mean_df = mean(df(1:6));
+    std_df = stf(df(1:6));
 end
