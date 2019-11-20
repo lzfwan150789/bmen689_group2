5535df3731ee281c4929f451884dc6b1b6e5ae60
Ronald Juarez <rmjch91@gmail.com>
2019-11-20 01:17:38 -0600

    mergin all changes

--- featureExtraction.m
+++ featureExtraction.m
@@ -3,15 +3,8 @@ function [featureVector] = featureExtraction(binaryMask, interior, boundary)
      area = Area(binaryMask);
      glcm = GLCM(interior);
      circularity = Circularity(boundary);
-<<<<<<< HEAD
-     %fractalTexture = getFractalTexture(interior);
-     fractalBoundary = getFractalBoundary(boundary);
-     cnBoundary = getCNBoundary(boundary);
-     featureVector = [circularity elongation, area, glcm, fractalBoundary, cnBoundary];
-=======
      fractalTexture = getFractalTexture(interior);
      fractalBoundary = getFractalBoundary(boundary);
      cnBoundary = getCNBoundary(boundary);
      featureVector = [circularity elongation, area, glcm,fractalTexture, fractalBoundary, cnBoundary];
->>>>>>> master
 end
